cd18943eb871efd605f91239fdda03792970dff5
zouyuhang <121839627+zouyuhang233@users.noreply.github.com>
2026-07-21 12:32:34 +0800

    upload: headfile.h

---
+++ cw_wg/APP/headfile.h
@@ -0,0 +1,15 @@
+#ifndef __HEAFILE_H_
+#define __HEAFILE_H_
+#include "usart.h"
+#include "spi.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdint.h"
+#include "w5500.h"
+#include "wizchip_conf.h"
+#include "socket.h"
+#include "int_w5500.h"
+
+
+
+#endif
