9f737a605d50321a23e35d8c4b790b404d15eba7
zouyuhang <121839627+zouyuhang233@users.noreply.github.com>
2026-07-21 12:28:33 +0800

    upload: headfile.h

---
+++ cw_dinweiqi/APP/headfile.h
@@ -0,0 +1,20 @@
+#ifndef __HEADFILE_H
+#define __HEADFILE_H
+
+#include "gpio.h"
+#include "main.h"
+
+#include "string.h"
+#include "stdint.h"
+#include "stdio.h"
+
+#include "usart.h"
+#include "fun.h"
+#include "Debug.h"
+#include "Com_Tool.h"
+#include "INT_AT6558R.h"
+#include "i2c.h"
+#include "ADXL345.h"
+#include "MN316.h"
+#include "tcp_send.h"
+#endif
